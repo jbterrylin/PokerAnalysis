88c882c7993ee9530dc7aedc3cd11cd7e6926916
Jbterrylin <wanglinlee1707@gmail.com>
2022-11-07 23:39:35 +0800

    fix multiple showdown will minus hero sumBets repeatly

--- Helper/HeroMoneyChange.py
+++ Helper/HeroMoneyChange.py
@@ -8,9 +8,8 @@ def heroMoneyChange(game):
 		for sd in game.showDown:
 			heroCollect = next((g for g in sd if g.player == HERO and g.move == Move.COLLECT), None)
 			if heroCollect is not None:
-				game.heroMoneyChange += heroCollect.money - game.sumBets[HERO]
-			else:
-				game.heroMoneyChange += -game.sumBets[HERO]
+				game.heroMoneyChange += heroCollect.money
+		game.heroMoneyChange += -game.sumBets[HERO]
 		if game.heroMoneyChange > 0:
 			game.heroResult = WIN
 		elif game.heroMoneyChange < 0:
